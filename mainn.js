f8213455a9b34f07272e38ff1939f1d24e4eef12
Abhishek12589 <hindujaabhishek5@gmail.com>
2023-03-08 10:00:49 +0530

    Our first Api

--- mainn.js
+++ mainn.js
@@ -2,6 +2,7 @@ var express = require("express")
 var fs = require("fs")
 
 var app = express()
+app.use(express.json())
 
 app.get("/", function(req,res){
     fs.readFile("./todo/indexing.html", "utf-8", function(err,data){
@@ -15,6 +16,34 @@ app.get("/scriptt.js", function(req,res){
     })
 })
 
+app.post("/save", function(req,res){
+    
+    fs.readFile("./db.txt", "utf-8", function(err,data){
+        var todos = []
+
+        if (data.length > 0){
+            todos = JSON.parse(data)
+        }
+        todos.push(req.body)
+
+        fs.writeFile("./db.txt", JSON.stringify(todos), function(err){
+            if (err){
+                res.end("Error Ocurred")
+            }
+            else{
+                res.end()
+            }
+        })
+    })
+})
+
+app.get("/todo", function(req,res){
+    fs.readFile("./db.txt", "utf-8", function(err,data){
+        res.end(data)
+    })
+})
+
 app.listen(3000, function(){
     console.log("Server is running on 3000")
 })
+
--- todo/scriptt.js
+++ todo/scriptt.js
@@ -1,1 +1,42 @@
-console.log("Hello Worldd")
+var input = document.getElementById("input")
+var btn = document.getElementById("btn")
+
+var parent = document.getElementById("parent")
+
+btn.addEventListener("click", function(){
+    if (input.value){
+
+        var request = new XMLHttpRequest()
+        request.open("post", "/save")
+        request.setRequestHeader("Content-type", "application/json")
+        request.send(JSON.stringify({todo : input.value}))
+
+        request.addEventListener("load", function(){
+            var li = document.createElement("li")
+            li.innerHTML = input.value
+    
+            parent.appendChild(li)
+    
+            input.value = ""
+        })
+       
+    }
+})
+
+var request = new XMLHttpRequest()
+request.open("get", "/todo")
+request.send()
+
+request.addEventListener("load", function(){
+    var todos = JSON.parse(request.responseText)
+
+    todos.forEach(function(task){
+        var li = document.createElement("li")
+        li.innerHTML = task.todo
+    
+        parent.appendChild(li)
+    
+        input.value = ""
+    });
+
+})
